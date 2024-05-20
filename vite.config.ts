76eb5e70b181c5900cec8fe40d23a626f1900a66
kirillgladkov <crosfire2011@gmail.com>
2024-05-20 22:52:36 +0300

    chore: Add Vite configuration file

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [],
+  base: '/',
+  define: {
+    'process.env': {},
+    global: {}
+  }
+})
