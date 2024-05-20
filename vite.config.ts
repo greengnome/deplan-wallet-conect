ebb9ed8d20499e62f40f98f9dee6799d8de8146f
kirillgladkov <crosfire2011@gmail.com>
2024-05-20 23:08:34 +0300

    chore: Update base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
-  base: '/',
+  base: '/deplan-wallet-conect/',
   define: {
     'process.env': {},
     global: {}
